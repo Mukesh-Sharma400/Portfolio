10da700f9aed81206f6d8088b53ec196fa518576
Mukesh Sharma <83642159+Mukesh-Sharma400@users.noreply.github.com>
2023-02-28 00:58:44 +0530

    added social

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -90,6 +90,108 @@ const Contact = () => {
           </form>
         </div>
       </div>
+      <div class="py-5">
+        <div class="row g-4">
+          <div class="col-md">
+            <h1 class="text-center mb-4">Social Info</h1>
+            <ul class="list-group list-group-flush lead">
+              <li class="list-group-item">
+                <a
+                  className="text-decoration-none text-black"
+                  href="tel:+917021739604"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i class="bi bi-telephone-fill text-dark mx-1"></i>
+                  <span class="fw-bold">Phone:</span> (+91) 70217-39604
+                </a>
+              </li>
+              <li class="list-group-item">
+                <a
+                  className="text-decoration-none text-black"
+                  href="mailto:mksh400@gmail.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i class="bi bi-envelope-fill text-dark mx-1"></i>
+                  <span class="fw-bold">Gmail:</span> mksh400@gmail.com
+                </a>
+              </li>
+              <li class="list-group-item">
+                <a
+                  className="text-decoration-none text-black"
+                  href="https://www.linkedin.com/in/mukesh-sharma400"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i class="bi bi-linkedin text-dark mx-1"></i>
+                  <span class="fw-bold">LinkedIn:</span> mukesh-sharma400
+                </a>
+              </li>
+
+              <li class="list-group-item">
+                <a
+                  className="text-decoration-none text-black"
+                  href="https://github.com/Mukesh-Sharma400"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i class="bi bi-github text-dark mx-1"></i>
+                  <span class="fw-bold">GitHub:</span> Mukesh-Sharma400
+                </a>
+              </li>
+
+              <li class="list-group-item">
+                <a
+                  className="text-decoration-none text-black"
+                  href="https://www.facebook.com/Mukesh400f"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i class="bi bi-facebook text-dark mx-1"></i>
+                  <span class="fw-bold">Facebook:</span> Mukesh400f
+                </a>
+              </li>
+              <li class="list-group-item">
+                <a
+                  className="text-decoration-none text-black"
+                  href="https://www.instagram.com/mukesh_sharma400"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i class="bi bi-instagram text-dark mx-1"></i>
+                  <span class="fw-bold">Instagram:</span> mukesh_sharma400
+                </a>
+              </li>
+              <li class="list-group-item">
+                <a
+                  className="text-decoration-none text-black"
+                  href="https://twitter.com/mukesh_sharma36"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i class="bi bi-twitter text-dark mx-1"></i>
+                  <span class="fw-bold">Twitter:</span> mukesh_sharma36
+                </a>
+              </li>
+              <li class="list-group-item">
+                <a
+                  className="text-decoration-none text-black"
+                  href="https://www.google.com/maps/place/Ghansoli,+Navi+Mumbai,+Maharashtra/@19.1265698,72.9837973,14z/data=!3m1!4b1!4m6!3m5!1s0x3be7c0ae08fd4fcb:0xeb29360c730e957f!8m2!3d19.1253629!4d72.9992013!16zL20vMDkxeHNy"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i class="bi bi-geo-alt-fill text-dark mx-1"></i>
+                  <span class="fw-bold">Address:</span> Ghansoli, Navi Mumbai
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md img-fluid img-card w-50 d-none d-lg-block">
+            <div id="map"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
