bd321054353404b3bafd3a5092693190a42f66bb
Mukesh Sharma <83642159+Mukesh-Sharma400@users.noreply.github.com>
2023-04-30 22:59:11 +0530

    changed home layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import mern from "../assets/mern.png";
 import maukes from "../assets/maukes.png";
 import Resume from "../components/Resume";
@@ -33,40 +32,40 @@ const Home = () => {
             integrations, and databases.
           </p>
         </div>
-        <div className="col-xxl-8 px-4 py-5 mx-auto">
-          <div className="row align-items-center justify-content-center g-5 pt-5">
-            <div className="col-10 col-sm-8 col-lg-6">
-              <img
-                src={maukes}
-                className="d-block mx-lg-auto img-fluid"
-                alt="maukes"
-                loading="lazy"
-              />
-            </div>
-            <div className="col-lg-6">
-              <h1 className="display-5 fw-bold lh-1 mb-3">
-                Who is <span className="text-danger">Mukesh</span> ?
-              </h1>
-              <p className="lead">
-                I started coding in 2020 at the age of 19. I learned the MERN
-                Stack in 2022. I believe people don't have to be a straight 'A'
-                student or a genius to learn to code.
-              </p>
-              <div className="d-grid gap-2 d-md-flex justify-content-center">
-                <Link
-                  to="/projects"
-                  className="btn btn-primary btn-lg px-4 me-md-2"
-                >
-                  See Projects Now !
-                </Link>
-                <Link
-                  to="/contact"
-                  className="btn btn-outline-warning btn-lg px-4 me-md-2"
-                >
-                  Contact Me ?
-                </Link>
-              </div>
-            </div>
+        <div class="row align-items-md-center py-5 g-4">
+          <div class="col-md-4 d-md-block">
+            <img
+              src={maukes}
+              className="d-block mx-lg-auto img-fluid"
+              alt="maukes"
+              loading="lazy"
+            />
+          </div>
+          <div class="col-md-8 col-sm-12">
+            <h1 className="display-5 fw-bold lh-1 mb-3">
+              Who is <span className="text-danger">Mukesh</span> ?
+            </h1>
+            <p className="text-start">
+              Hello, my name is Mukesh Sharma and I am a MERN Stack developer. I
+              have extensive experience in building web applications using
+              MongoDB, Express, React, and Node.js. I am passionate about
+              creating efficient and scalable applications that deliver a great
+              user experience. I specialize in developing RESTful APIs, handling
+              database integration, and implementing client-side functionality
+              with React. I also have experience with other popular technologies
+              such as Redux, Sass, and Bootstrap. Throughout my career, I have
+              worked with a variety of clients and industries, ranging from
+              startups to large corporations. This has given me the ability to
+              adapt to different project requirements and work with diverse
+              teams. I am a self-motivated individual who is always looking for
+              ways to improve my skills and learn new technologies. I am an
+              excellent problem solver and I thrive in challenging situations. I
+              believe in the importance of clean code and always strive to write
+              maintainable and reusable code. If you are looking for a MERN
+              Stack developer who can bring your ideas to life, please don't
+              hesitate to contact me. I am always open to new opportunities and
+              collaborations.
+            </p>
           </div>
         </div>
       </div>
