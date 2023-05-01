98edaa46c0808129cdd51a8b971f49df72fb3232
Mukesh Sharma <83642159+Mukesh-Sharma400@users.noreply.github.com>
2023-05-01 19:44:02 +0530

    profile summary updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -46,25 +46,18 @@ const Home = () => {
               Who is <span className="text-danger">Mukesh</span> ?
             </h1>
             <p className="big--para">
-              Hi, my name is Mukesh Sharma and I am a MERN Stack developer. I
-              have extensive experience in building web applications using
-              MongoDB, Express, React, and Node.js. I am passionate about
-              creating efficient and scalable applications that deliver a great
-              user experience. I specialize in developing RESTful APIs, handling
-              database integration, and implementing client-side functionality
-              with React. I also have experience with other popular technologies
-              such as Redux, Sass, and Bootstrap. Throughout my career, I have
-              worked with a variety of clients and industries, ranging from
-              startups to large corporations. This has given me the ability to
-              adapt to different project requirements and work with diverse
-              teams. I am a self-motivated individual who is always looking for
-              ways to improve my skills and learn new technologies. I am an
-              excellent problem solver and I thrive in challenging situations. I
-              believe in the importance of clean code and always strive to write
-              maintainable and reusable code. If you are looking for a MERN
-              Stack developer who can bring your ideas to life, please don't
-              hesitate to contact me. I am always open to new opportunities and
-              collaborations.
+              My name is Mukesh Sharma and I'm a MERN Stack developer with a
+              passion for building efficient and scalable web applications. With
+              expertise in MongoDB, Express, React, and Node.js, I specialize in
+              developing RESTful APIs, integrating databases, and implementing
+              client-side functionality using React. I have experience with
+              Redux, Sass, and Bootstrap, and have worked with clients from
+              startups to large corporations, adapting to diverse teams and
+              project requirements. As a self-motivated problem solver, I strive
+              to write maintainable and reusable code. I'm always looking for
+              opportunities to improve my skills and learn new technologies. If
+              you're looking for a MERN Stack developer to bring your ideas to
+              life, let's collaborate!
             </p>
           </div>
         </div>
