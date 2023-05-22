0690715970ec5fee3af2084ff282ea974f61bc92
Mukesh Sharma <83642159+Mukesh-Sharma400@users.noreply.github.com>
2023-05-22 20:03:46 +0530

    improvements

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,9 +7,11 @@ const Resume = () => {
         href="https://drive.google.com/file/d/1Ii98iSskvvSQ1qBaUMTO_RrnqN5FE-i0/view?usp=sharing"
         target="_blank"
         rel="noopener noreferrer"
+        alt="download_resume"
       >
         <button
           type="button"
+          aria-label="download_resume"
           className="btn btn-primary btn-lg rounded-pill button"
         >
           <span>
